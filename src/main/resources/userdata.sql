477a11e8f2e1c2396125987c8f1abe0bb1924101
Simran Agrawal <simranagrawal1603@gmail.com>
2021-07-19 15:41:32 +0530

    auth commit

---
+++ src/main/resources/userdata.sql
@@ -0,0 +1,4 @@
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('Swathi','password','teamlead');
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('Manish','password','teamlead');
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('simran','password','teamlead');
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('Himanshu','password','teamlead');
